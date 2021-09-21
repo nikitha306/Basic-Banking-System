79be6805b1792191582975cfc64bbb51d03dff30
nikitha306 <71863076+nikitha306@users.noreply.github.com>
2021-09-21 20:23:38 +0530

    Add files via upload

--- sql/sparksbank.sql
+++ sql/sparksbank.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 02, 2020 at 08:02 AM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.6
+-- Generation Time: Sep 21, 2021 at 04:52 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -35,6 +35,20 @@ CREATE TABLE `transaction` (
   `datetime` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `transaction`
+--
+
+INSERT INTO `transaction` (`sno`, `sender`, `receiver`, `balance`, `datetime`) VALUES
+(0, 'Ira', 'Kabir', 6470, '2021-09-19'),
+(0, 'Ram', 'Nikitha', 8900, '2021-09-19'),
+(0, 'Ved', 'John', 6548, '2021-09-19'),
+(0, 'Ram', 'Kabir', 6554, '2021-09-20'),
+(0, 'Nikitha', 'Shreya', 5699, '2021-09-20'),
+(0, 'Nikitha', 'Adhira', 5600, '2021-09-20'),
+(0, 'Naina', 'Ira', 6000, '2021-09-20'),
+(0, 'Shreya', 'Naina', 8709, '2021-09-20');
+
 -- --------------------------------------------------------
 
 --
@@ -53,16 +67,16 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `balance`) VALUES
-(1, 'Philona ', 'philona@gmail.com', 5000),
-(2, 'Nivya', 'nivya@gmail.com', 6000),
-(3, 'Devika', 'devika@gmail.com', 3000),
-(4, 'Neeraja', 'neeraja@gmail.com', 5000),
-(5, 'Sreya', 'sreya@gmail.com', 2000),
-(6, 'Nisha', 'nisha@gmail.com', 6000),
-(7, 'Irene', 'irene@gmail.com', 7000),
-(8, 'Tom', 'tom@gmail.com', 5000),
-(9, 'Jerry', 'jerry@gmail.com', 1000),
-(10, 'Justin', 'justin@gmail.com', 9000);
+(1, 'Nikitha', 'nikithab@gmail.com', 12601),
+(2, 'Adhira', 'lionessadhi@gmail.com', 9000),
+(3, 'Ved', 'ved@hotmail.com', 2452),
+(4, 'Ram', 'ram@outlook.com', 64546),
+(5, 'Shreya', 'sh32@hotmail.com', 4990),
+(6, 'Naina', 'naina90@hotmail.com', 92709),
+(7, 'Ira', 'ira34@gmail.com', 8295),
+(8, 'Clarie', 'clarie56@gmail.com', 8526),
+(9, 'Kabir', 'kabir@outlook.com', 89546),
+(10, 'John', 'jo78@hotmail.com', 61225);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
